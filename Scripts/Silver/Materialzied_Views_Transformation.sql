86952987fb8bfce537d94226d1717a58b56fa9dd
Omama Mashhood <122161841+Omamaa16@users.noreply.github.com>
2026-07-29 13:44:45 +0200

    Update Materialzied_Views_Transformation.sql

--- Scripts/Silver/Materialzied_Views_Transformation.sql
+++ Scripts/Silver/Materialzied_Views_Transformation.sql
@@ -6,8 +6,8 @@ together for the documentation
 The expectations would fail the udate primarily due to the fact that the pk
 is duplicated; in case of further data violations, data would be quarantined
 
-Full naming convention has not been used since the configuration was set up
-in the lakeflow pipeline UI*/
+Full naming convention has not been used since the configuration had been set 
+up in the lakeflow pipeline UI*/
 -----------------------------------------------------------------------------
 
 -------------------------------------------------------------------------------
